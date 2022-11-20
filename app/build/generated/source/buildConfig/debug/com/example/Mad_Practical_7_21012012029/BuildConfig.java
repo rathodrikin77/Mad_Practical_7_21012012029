007b63a5239c0afeb65c809ae532ca7df677d0f7
Rikin Rathod <rikinrathod77@gmail.com>
2022-11-21 01:24:44 +0530

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/example/Mad_Practical_7_21012012029/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.Mad_Practical_7_21012012029;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.Mad_Practical_7_21012012029";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
